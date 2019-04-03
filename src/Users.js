885379d6794edcaba5fe52c19e1d99d9b28dde09
unknown <taylorwcolten@gmail.com>
2019-04-02 23:44:46 -0500

    commit

--- src/Users.js
+++ src/Users.js
@@ -21,10 +21,38 @@ class Users extends Component {
           <Button onClick={this.getUsers} variant="contained" color="primary">Get Users</Button>
         </div>
         <div className="users-block">
+        {this.props.users.map(user => (
+            <form key={user.id}>
+              <ul className="square-box" style={styles}>
+                <li>name: {user.name}</li>
+                <li>username: {user.username}</li>
+                <li>email: {user.email}</li>
+                <li>website: {user.website}</li>
+              </ul>
+            </form>
+          ))}
         </div>
       </Fragment>
     )
   }
 }
 
-export default Users
+const mapStateToProps = state => {
+  return {
+    users: state.users
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setUsers: displayUsers =>
+      dispatch({
+        type: "SET_USERS",
+        value: displayUsers
+      })
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Users);
